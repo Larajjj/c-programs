e1c4ff28661e217c385a421d55d6c839e1a98ce0
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-11 10:43:04 +0530

    Create compare2.c

---
+++ compare2.c
@@ -0,0 +1,14 @@
+
+ what are the different ways to debug a logical error?       
+ 1.Comparing with the existing program
+     problem: strategy to learn a program not a learn programming.
+	     
+ 2.Mental tracing 
+     problem:If understanding is wrong, tracing goes wrong.
+
+ 3.printf
+    problem: We cannot find the control flow.
+ 
+ ddd-data display debugger
+ 
+ debugging:
