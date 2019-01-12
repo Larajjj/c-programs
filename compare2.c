fcdb038c8c2013607006fab2c39e51f746085895
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-12 11:45:06 +0530

    Update compare2.c

--- compare2.c
+++ compare2.c
@@ -90,15 +90,55 @@ Program terminated with signal SIGABRT, Aborted.
 
 
 
+#include <stdio.h>
+int input(int *a, int *b)
+{
+  printf("Enter a number\n");
+  scanf("%d",a);
+  printf("Enter a number\n");
+  scanf("%d",b);
+}
+int compare(int a, int b)
+{
+   int c;
+   if(a<b){
+     c=b;
+}
+   else{
+     c=a;
+}
+   return c;
+}
+void output(int a, int b, int c)
+{
+   printf("%d is smaller among %d and %d",c,a,b);
+}
+int main()
+{
+   int x,y,z;
+   input(&x,&y);
+   z=compare(x,y);
+   output(x,y,z);
+}
 
-
-
+Enter a number
+23
+Enter a number
+36
+36 is smaller among 23 and 36nvidia@nvidia-OptiPlex-3050:~$ 
 
 
 
 
 what is a logical error?
-
+	A logic error (or logical error) is a 'bug' or mistake in a program's 
+source code that results in incorrect or unexpected behaviour. It is a type of runtime
+error that may simply produce the wrong output or may cause a program to crash while running. 
+Many different types of programming mistakes can cause logic errors.
+	A logic error (or logical error) is a mistake in a program's source code that
+results in incorrect or unexpected behavior.
+Many different types of programming mistakes can cause logic errors.
+	
 
 
  what are the different ways to debug a logical error?       
@@ -111,9 +151,16 @@ what is a logical error?
  3.printf
     problem: We cannot find the control flow.
  
- ddd-data display debugger
+ 4.ddd-data display debugger
  
  
+ Debugging:
 
+	Writing code is only the beginning of completing a programming project. 
+After the original implementation is complete, it is time to test the program. Unfortunately, 
+only the rare (and usually non-priority) code project is completed without a single bug. 
+Hence, debugging takes on great importance: the earlier you find an error, the less it will cost.
+A major bug found before distribution is much, much cheaper to fix than a major bug found by thousands of your users.
 
-    
+	A debugger allows you, the programmer, to interact and inspect the running program, making it possible to 
+trace the flow of execution and track down the problems. 
