817f23d7e4a0fd0e2d00c52e298a5a40d3bb20d4
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-12 09:46:52 +0530

    Update compare2.c

--- compare2.c
+++ compare2.c
@@ -46,36 +46,6 @@ what is a logical error?
 
 
 
-#include <stdio.h>
-int input(int *a, int *b)
-{
-  printf("Enter a number\n");
-  scanf("%d",a);
-  printf("Enter a number\n");
-  scanf("%d",b);
-}
-int compare(int a, int b)
-{
-   int c;
-   if(a<b){
-     c=a;
-}
-   else{
-     c=b;
-}
-   return c;
-}
-void output(int a, int b, int c)
-{
-   printf("%d is smaller among %d and %d",c,a,b);
-}
-int main()
-{
-   int x,y,z;
-   input(&x,&y);
-   z=compare(x,y);
-   output(x,y,z);
-}
 
 
 
@@ -103,111 +73,7 @@ int main()
  
  ddd-data display debugger
  
- debugging:
-
-#include <stdio.h>
-int input(int *a, int *b)
-{
-  printf("Enter a number\n");
-  scanf("%d",a);
-  printf("Enter a number\n");
-  scanf("%d",b);
-}
-int compare(int a, int b)
-{
-   int c;
-   if(a<b){
-     c=a;
-}
-   else{
-     c=b;
-}
-   return c;
-}
-void output(int a, int b, int c)
-{
-   printf("%d is smaller among %d and %d",c,a,b);
-}
-int main()
-{
-	
-	
-#include <stdio.h>
-int input(int *a, int *b)
-{
-  printf("Enter a number\n");
-  scanf("%d",a);
-  printf("Enter a number\n");
-  scanf("%d",b);
-}
-int compare(int a, int b)
-{
-   int c;
-   if(a<b){
-     c=a;
-}
-   else{
-     c=b;
-}
-   return c;
-}
-void output(int a, int b, int c)
-{
-   printf("%d is smaller among %d and %d",c,a,b);
-}
-int main()
-{
-#include <stdio.h>
-int input(int *a, int *b)
-{
-  printf("Enter a number\n");
-  scanf("%d",a);
-  printf("Enter a number\n");
-  scanf("%d",b);
-}
-int compare(int a, int b)
-{
-   int c;
-   if(a<b){
-     c=a;
-}
-   else{
-     c=b;
-}
-   return c;
-}
-void output(int a, int b, int c)
-{
-   printf("%d is smaller among %d and %d",c,a,b);
-}
-int main()
-{
-	
-	#include <stdio.h>
-int input(int *a, int *b)
-{
-  printf("Enter a number\n");
-  scanf("%d",a);
-  printf("Enter a number\n");
-  scanf("%d",b);
-}
-int compare(int a, int b)
-{
-   int c;
-   if(a<b){
-     c=a;
-}
-   else{
-     c=b;
-}
-   return c;
-}
-void output(int a, int b, int c)
-{
-   printf("%d is smaller among %d and %d",c,a,b);
-}
-int main()
-{
+ 
 
 
     
