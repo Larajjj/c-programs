9e3ce081654edc6b07a48577c13c1b1799ee6d50
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-14 15:46:28 +0530

    Update 3compare1.c

--- 3compare1.c
+++ 3compare1.c
@@ -128,3 +128,5 @@ how to initialise array using array literals
 how to declare an array 
 how to get the value of array element
 how to declare the value of array element
+how to return the value of array?
+what is variable length array?
