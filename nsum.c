4e5afaec6c4861d9e57f2e3c6e8de649855f8e6e
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-16 19:02:01 +0530

    Update nsum.c

--- nsum.c
+++ nsum.c
@@ -4,7 +4,7 @@ int main()
         int n,i,sum=0;
         printf("how many numbers you want to add:");
         scanf("%d",&n);
-        for (i=1; i<=n; i++)
+        for (i=0; i<=n; i++)
         {
         sum+=i;
         }
@@ -12,3 +12,21 @@ int main()
 
         return 0;
 }
+
+
+
+#include <stdio.h>
+int main()
+int input()
+{
+        int n,i,sum=0;
+        printf("enter the numbers you want to add:");
+        scanf("%d",&n);
+        for (i=0; i<=n; i++)
+        return n;
+}
+int compare{
+        sum+=i;
+        printf("sum is %d",sum);
+}
+
