be97d698c850fa194e0f36b7fb2dc1aa3869b8c4
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-01-24 18:24:29 +0530

    Update nsum.c

--- nsum.c
+++ nsum.c
@@ -52,3 +52,42 @@ void output()
         int sum;
         printf("%d is the sum of n different numbers",sum);
 }
+
+
+
+
+
+#include <stdio.h>
+int input()
+{
+        int n;
+        printf("enter the number of values you want to add");
+        scanf("%d",&n);
+        return n;
+}
+int compute(int n)
+{
+        int i,add[n],sum=0;
+        printf("enter numbers to be added:");
+        for(i=0;i<n;i++)
+        {
+                scanf("%d",&add[i]);
+        }
+        for (i=0;i<n;i++)
+        {
+                sum=sum+add[i];
+        }
+        return sum;
+}
+void output(int sum)
+{
+        printf("%d is the sum of n different numbers",sum);
+}
+int main()
+{
+        int n,s;
+        n=input();
+        s=compute(n);
+        output(s);
+        return 0;
+}
