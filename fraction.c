dcaeef632a3d3613053e064b3deedb0bb5a395bd
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-02-01 19:09:28 +0530

    Update fraction.c

--- fraction.c
+++ fraction.c
@@ -8,3 +8,26 @@ struct fraction get_fraction_by_ret()
 {
   struct fraction f;
   printf(
+#include <stdio.h>
+struct fraction
+{
+        int numerator;
+        int denominator;
+};
+void input_fraction(struct fraction *f,struct fraction *g)
+{
+        printf("enter num 1:");
+        scanf("%d",&f->numerator);
+        printf("enter num 2:");
+        scanf("%d",&f->numerator);
+        printf("enter den 1:");
+        scanf("%d",&g->denominator);
+        printf("enter den 2:");
+        scanf("%d",&g->denominator);
+}
+void add_fraction(struct fraction,struct fraction)
+{
+        int h;
+        fraction->h=(f-> numerator*g-> denominator + g-> numerator*f-> denominator)
+        return h;
+                                                              13,1-8        Top
