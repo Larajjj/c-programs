a8d6f95502c1aea4f750fd1715e6ad607cb8eec5
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-03-06 18:50:08 +0530

    Update array.n.c

--- array.n.c
+++ array.n.c
@@ -1,22 +1,30 @@
 
 #include <stdio.h>
-int input()
+void input()
 {
-        int i,add[n],sum=0;
+        int n;
+        int i,add[n];
         printf("enter numbers to be added:");
+        scanf("%d",&n);
         for(i=0;i<n;i++)
         {
                 scanf("%d",&add[i]);
         }
 }
-int compute(int n,add)
+int compute(int n,int add[n])
 {
+        int i,sum=0;
         for (i=0;i<n;i++)
         {
                 sum=sum+add[i];
         }
+        return sum;
 }
 void output(int sum)
 {
-        printf("%d is the sum of n different numbers",sum);
+          printf("%d is the sum of n different numbers",sum);
 }
+int main()
+{
+}
+
