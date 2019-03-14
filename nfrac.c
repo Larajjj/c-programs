a71f419ef443f650e8ebb750d977e8a2f46b6057
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-03-14 18:49:42 +0530

    Update nfrac.c

--- nfrac.c
+++ nfrac.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
+struct fraction()
+{
+        int numerator;
+        int denominator;
+}
+int number()
+{
+        struct numerator a,struct denominator b;
+        printf("enter the number of numerators to be added");
+        scanf("%d",&a);
+        printf("enter the number of denominators to be added");
+        scanf("%d",&b);
+}
+void input(int n,struct f[n],struct g[n])
+{
+        int i;
+        for (i=0;i<n;i++)
+        {
+                scanf("%d",&f[n]);
+        }
+        for (i=0;i<n;i++)
+        {
+                scanf("%d",&g[n]);
+        }
+}
 
