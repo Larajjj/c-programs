5ad36a71bdd63937afdfcade22539b93e1b7a070
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-03-15 18:28:47 +0530

    Update nfrac.c

--- nfrac.c
+++ nfrac.c
@@ -12,16 +12,4 @@ int number()
         printf("enter the number of denominators to be added");
         scanf("%d",&b);
 }
-void input(int n,struct f[n],struct g[n])
-{
-        int i;
-        for (i=0;i<n;i++)
-        {
-                scanf("%d",&f[n]);
-        }
-        for (i=0;i<n;i++)
-        {
-                scanf("%d",&g[n]);
-        }
-}
 
