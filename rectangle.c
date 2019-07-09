67ea00149ceba939c47389ed7105b9b01d5063c7
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-07-09 15:04:48 +0530

    Update rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -5,7 +5,7 @@ struct v
         float x;
         float y;
 };
-struct_rectangle
+struct rectangle
 {
         struct v v[3];
         float l;
@@ -19,3 +19,23 @@ int input1()
         scanf("%d",&n);
         return n;
 }
+void input2(int n,struct rectangle a[20])
+{
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        printf("Enter the x coordinate of v %d",j+1);
+                        scanf("%f",&a[i].v[j].x);
+                        printf("Enter the y coordinate of v %d",j+1);
+                        scanf("%f",&a[i].v[j].y);
+                        printf("%f,%f\n",a[i].v[j].x,a[i].v[j].y);
+                }
+        }
+}
+int main()
+{
+}
+
+                                                                                                                            1,1           Top
