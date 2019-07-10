2666887dd1161078ac800fdc87a537c2bff7198c
Larajjj <42802765+Larajjj@users.noreply.github.com>
2019-07-10 12:19:24 +0530

    Update rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -73,3 +73,88 @@ int main()
 {
 }
 
+
+
+
+#include<stdio.h>
+#include<math.h>
+struct v
+{
+        float x;
+        float y;
+};
+struct rectangle
+{
+        struct v v[3];
+        float l;
+        float b;
+        float a;
+};
+int input1()
+{
+        int n;
+        printf("Ener the value of n:");
+        scanf("%d",&n);
+        return n;
+}
+void input2(int n,struct rectangle a[])
+{
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        printf("Enter the x coordinate of v %d",j+1);
+                        scanf("%f",&a[i].v[j].x);
+                        printf("Enter the y coordinate of v %d",j+1);
+                        scanf("%f",&a[i].v[j].y);
+                        printf("%f,%f\n",a[i].v[j].x,a[i].v[j].y);
+                }
+        }
+
+}
+void distance(float x1,float x2,float y1,float y2,float x3,float y3)
+{
+
+        float d1,d2;
+        d1=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        d2=sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1));
+
+}
+/*void compute1(int n, struct rectangle a[])
+{
+        int i;
+        for(i=0;i<n;i++)
+        {
+                int j=0;
+                a[i].l=distance(a[i].v[j].x,a[i].v[j+1].x,a[i].v[j].y,a[i].v[j+1].y);
+                a[i].b=distance(a[i].v[j+1].x,a[i].v[j+2].x,a[i].v[j+1].y,a[i].v[j+2].y);
+        }
+}*/
+
+void compute(float area)
+{
+       // printf("l=%f,b=%f",a[i].l,a[i].b);//
+       // a[i].a=a[i].l*a[i].b;//
+       area=d1*d2;
+
+}
+void output(int n,float)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("area of %d rectangle is %f",i+1,a[i].a);
+    }
+}
+int main()
+{
+    int n;
+    struct rectangle a[100];
+    n=input1();
+    input2(n,a);
+    compute2(n,a);
+    compute2(n,a);
+    output(n,a);
+}
+
